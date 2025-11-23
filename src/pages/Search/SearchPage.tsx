47a124c256d381fd11a2b7cf2771f957855ea21f
dragusheen <nathan.tirolf@epitech.eu>
2025-11-23 17:29:53 -0500

    [~] Rename pages components

--- src/pages/Search/SearchPage.tsx
+++ src/pages/Search/SearchPage.tsx
@@ -13,13 +13,17 @@
 
 /* ----- IMPORTS ----- */
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 
 /* ----- COMPONENT ----- */
 const SearchPage: React.FC = () => {
 	return (
-		<div className="flex justify-center items-center h-screen">
+		<div className="flex flex-col gap-16 justify-center items-center h-screen">
 			<h1 className="textStyle-title">Welcome to Search page</h1>
+			<NavLink to="/emission" className="textStyle-subtitle color-mountain-meadow underline">
+				Go to Emission Page
+			</NavLink>
 		</div>
 	);
 };
