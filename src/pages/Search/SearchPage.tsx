5f969535621f71dc2cae92ccf5ac1ffb9edd1cb9
dragusheen <nathan.tirolf@epitech.eu>
2025-11-25 21:30:52 -0500

    [+] Display podcast on search page

--- src/pages/Search/SearchPage.tsx
+++ src/pages/Search/SearchPage.tsx
@@ -10,20 +10,43 @@
 	--U-----U------------------------
 */
 
-
 /* ----- IMPORTS ----- */
+import { getEmissions } from "@/data/TemporaryData";
 import React from "react";
-import { NavLink } from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 /* ----- COMPONENT ----- */
 const SearchPage: React.FC = () => {
+	const emissions = getEmissions();
+
 	return (
-		<div className="flex flex-col gap-16 justify-center items-center h-screen">
-			<h1 className="textStyle-title">Welcome to Search page</h1>
-			<NavLink to="/emission" className="textStyle-subtitle color-mountain-meadow underline">
-				Emission example
-			</NavLink>
+		<div className="flex flex-col gap-8 p-8 min-h-screen">
+			<h1 className="textStyle-title text-center">All Emissions</h1>
+
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+				{emissions.map((emission) => (
+					<Link
+						key={emission.id}
+						to={`/emission/${emission.id}`}
+						className="block bg-background-bangladesh-green rounded-2xl overflow-hidden hover:scale-105 transition-transform duration-200"
+					>
+						<img
+							src={emission.image}
+							alt={emission.name}
+							className="w-full aspect-square object-cover"
+						/>
+
+						<div className="p-4 flex flex-col gap-2">
+							<div className="textStyle-subtitle color-anti-flash-white truncate">
+								{emission.name}
+							</div>
+							<div className="textStyle-text color-anti-flash-white line-clamp-3">
+								{emission.description}
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 };
