57a009136ae15331e1bff13c713728e9b6a51f8e
dragusheen <nathan.tirolf@epitech.eu>
2025-11-27 14:50:56 -0500

    [~] Isolate SocialNetwork

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -12,34 +12,12 @@
 
 /* ----- IMPORTS ----- */
 import React from "react";
+import { GetSocialNetworkDatas } from "@/data/SocialNetworkData";
+import DisplaySocialNetwork from "@/components/Display/SocialNetwork/DisplaySocialNetwork";
 
 
 /* ----- COMPONENT ----- */
 const Footer: React.FC = () => {
-	type SocialNetworkConfig = {
-		name: string,
-		ref: string,
-		accountName: string,
-	};
-
-	const SocialNetworks: SocialNetworkConfig[] = [
-		{
-			name: "Instagram",
-			ref: "https://www.instagram.com/studio52.20/",
-			accountName: "@Studio52.20"
-		},
-		{
-			name: "Spotify",
-			ref: "https://open.spotify.com/show/4FAq7xczKaPkyk5q2ZfeHA",
-			accountName: "Studio5220",
-		},
-		{
-			name: "Discord",
-			ref: "https://discord.com/invite/nB2CBCfS3",
-			accountName: "Studio 52.20",
-		},
-	];
-
 	return (
 		<div>
 			<div className={'bg-(--color-bangladesh-green) py-12 px-4 md:px-8 lg:px-16'}>
@@ -61,7 +39,7 @@ const Footer: React.FC = () => {
 							</div>
 							<div className="textStyle-text">
 								<a href="mailto:club_studio5220@uqac.ca" className="flex items-center gap-2">
-									<img src="/svg/outlookIcon.svg" alt="Email" className="w-5 h-5" />
+									<img src="/svg/socials/outlookIcon.svg" alt="Email" className="w-5 h-5" />
 									club_studio5220@uqac.ca
 								</a>
 							</div>
@@ -72,21 +50,7 @@ const Footer: React.FC = () => {
 								Réseaux sociaux
 							</div>
 							<div className="flex flex-col gap-4">
-								{
-									SocialNetworks.map(social =>
-										<div className="textStyle-text">
-											<a href={social.ref} className="flex items-center gap-2" target="_blank" rel="noopener noreferrer">
-												<img
-													src={`/svg/${social.name.toLowerCase()}Icon.svg`}
-													alt={`${social.name} Icon`} className="w-5 h-5"
-												/>
-												<p>
-													{social.name} : {social.accountName}
-												</p>
-											</a>
-										</div>
-									)
-								}
+								{GetSocialNetworkDatas().map((social, index) => <DisplaySocialNetwork key={index} social={social} />)}
 							</div>
 						</div>
 					</div>
