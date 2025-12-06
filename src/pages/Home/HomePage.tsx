273fba53bc137734081708cbaaa99cd4e1ab380d
dragusheen <nathan.tirolf@epitech.eu>
2025-12-06 14:20:55 -0500

    [~] Move search page content to home page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -10,18 +10,87 @@
 	--U-----U------------------------
 */
 
-
 /* ----- IMPORTS ----- */
-import React from "react";
-
+import DisplayCategorySmall from "@/components/Display/Category/DisplayCategorySmall";
+import DisplayEmissionMedium from "@/components/Display/Emission/DisplayEmissionMedium";
+import DisplaySerieSmall from "@/components/Display/Serie/DisplaySerieSmall";
+import { getCategories } from "@/store/CategoryStore";
+import { getRecentEmissions } from "@/store/EmissionStore";
+import { getSeries } from "@/store/SerieStore";
+import type { ICategory } from "@/type/Category";
+import type { IEmission } from "@/type/Emission";
+import type { ISerie } from "@/type/Serie";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import type SearchPage from "../Search/SearchPage";
 
 /* ----- COMPONENT ----- */
 const HomePage: React.FC = () => {
-	return (
-		<div className="flex justify-center items-center h-screen">
-			<h1 className="textStyle-title">Welcome to Studio 52.20</h1>
+	const [emissions, setEmissions] = useState<IEmission[]>([]);
+	const [series, setSeries] = useState<ISerie[]>([]);
+	const [categories, setCategories] = useState<ICategory[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+
+			const [emissionsRes, seriesRes, categoriesRes] = await Promise.all([
+				getRecentEmissions(8), getSeries(), getCategories()
+			]);
+
+			setEmissions(emissionsRes);
+			setSeries(seriesRes);
+			setCategories(categoriesRes);
+			setLoading(false);
+		};
+		fetchData();
+	}, []);
+
+	if (loading) {
+		return <div className="flex justify-center items-center h-screen textStyle-title">
+			Loading...
 		</div>
-	);
+	}
+
+	return emissions.length > 0 ?
+		<div className="flex flex-col gap-16 p-8 min-h-screen">
+			<div className="h-28 shrink-0"></div>
+
+			<div className="flex flex-col gap-2">
+				<div className="textStyle-title color-anti-flash-white">Recent Emissions</div>
+				<div className="flex overflow-x-auto gap-8 p-4">
+					{emissions.map((emission) => (
+						<Link to={`/emission/${emission.id}`} key={emission.id}>
+							<DisplayEmissionMedium emission={emission} />
+						</Link>
+					))}
+				</div>
+			</div>
+
+			<div className="flex flex-col gap-2">
+				<div className="textStyle-title color-anti-flash-white">Categories</div>
+				<div className="grid grid-rows-2 grid-flow-col gap-4 overflow-x-auto pb-4 px-1">
+					{categories.map((category) => (
+						<DisplayCategorySmall category={category} key={category.id} />
+					))}
+				</div>
+			</div>
+
+			<div className="flex flex-col gap-2">
+				<div className="textStyle-title color-anti-flash-white">Series</div>
+				<div className="flex overflow-x-auto gap-8 p-4">
+					{series.map((serie) => (
+						<DisplaySerieSmall serie={serie} key={serie.id} />
+					))}
+				</div>
+			</div>
+		</div >
+		:
+		<div className="flex justify-center items-center h-screen textStyle-title">
+			No emissions...
+		</div>
+
 };
 
 export default HomePage;
--- src/pages/Search/SearchPage.tsx
+++ src/pages/Search/SearchPage.tsx
@@ -11,85 +11,15 @@
 */
 
 /* ----- IMPORTS ----- */
-import DisplayCategorySmall from "@/components/Display/Category/DisplayCategorySmall";
-import DisplayEmissionMedium from "@/components/Display/Emission/DisplayEmissionMedium";
-import DisplaySerieSmall from "@/components/Display/Serie/DisplaySerieSmall";
-import { getCategories } from "@/store/CategoryStore";
-import { getRecentEmissions } from "@/store/EmissionStore";
-import { getSeries } from "@/store/SerieStore";
-import type { ICategory } from "@/type/Category";
-import type { IEmission } from "@/type/Emission";
-import type { ISerie } from "@/type/Serie";
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
 /* ----- COMPONENT ----- */
 const SearchPage: React.FC = () => {
-	const [emissions, setEmissions] = useState<IEmission[]>([]);
-	const [series, setSeries] = useState<ISerie[]>([]);
-	const [categories, setCategories] = useState<ICategory[]>([]);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			setLoading(true);
-
-			const [emissionsRes, seriesRes, categoriesRes] = await Promise.all([
-				getRecentEmissions(8), getSeries(), getCategories()
-			]);
-
-			setEmissions(emissionsRes);
-			setSeries(seriesRes);
-			setCategories(categoriesRes);
-			setLoading(false);
-		};
-		fetchData();
-	}, []);
-
-	if (loading) {
-		return <div className="flex justify-center items-center h-screen textStyle-title">
-			Loading...
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<h1 className="textStyle-title">Welcome to Search page</h1>
 		</div>
-	}
-
-	return emissions.length > 0 ?
-		<div className="flex flex-col gap-16 p-8 min-h-screen">
-			<div className="h-28 shrink-0"></div>
-
-			<div className="flex flex-col gap-2">
-				<div className="textStyle-title color-anti-flash-white">Recent Emissions</div>
-				<div className="flex overflow-x-auto gap-8 p-4">
-					{emissions.map((emission) => (
-						<Link to={`/emission/${emission.id}`} key={emission.id}>
-							<DisplayEmissionMedium emission={emission} />
-						</Link>
-					))}
-				</div>
-			</div>
-
-			<div className="flex flex-col gap-2">
-				<div className="textStyle-title color-anti-flash-white">Categories</div>
-				<div className="grid grid-rows-2 grid-flow-col gap-4 overflow-x-auto pb-4 px-1">
-					{categories.map((category) => (
-						<DisplayCategorySmall category={category} key={category.id} />
-					))}
-				</div>
-			</div>
-
-			<div className="flex flex-col gap-2">
-				<div className="textStyle-title color-anti-flash-white">Series</div>
-				<div className="flex overflow-x-auto gap-8 p-4">
-					{series.map((serie) => (
-						<DisplaySerieSmall serie={serie} key={serie.id} />
-					))}
-				</div>
-			</div>
-		</div >
-		:
-		<div className="flex justify-center items-center h-screen textStyle-title">
-			No emissions...
-		</div>
-
+	);
 };
 
 export default SearchPage;
